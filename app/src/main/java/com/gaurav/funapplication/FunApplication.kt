b32b63474172896e1aa3f499f3fa5e093fc164ee
gauravkumar <i.gauravdubey@gmail.com>
2025-02-19 11:32:24 +0530

    Commiting the initial setup of App

---
+++ app/src/main/java/com/gaurav/funapplication/FunApplication.kt
@@ -0,0 +1,8 @@
+package com.gaurav.funapplication
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class FunApplication:Application() {
+}
---
+++ app/src/main/java/com/gaurav/funapplication/presentation/activity/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.gaurav.funapplication.presentation.activity
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import com.gaurav.funapplication.presentation.theme.FunApplicationTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            FunApplicationTheme {
+                /*Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+
+                }*/
+            }
+        }
+    }
+}
