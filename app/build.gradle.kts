b39a3f107e2018b925547816171771702d9c434d
gauravkumar <i.gauravdubey@gmail.com>
2025-03-01 01:46:06 +0530

    Added the google service plugin in libs.version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
     alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.ksp)
     alias(libs.plugins.hilt)
-    id("com.google.gms.google-services")
+    alias(libs.plugins.google.services)
 }
 
 android {
